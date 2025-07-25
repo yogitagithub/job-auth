2c3594406e61eac97ee572605ffa876d7459874a
yogitagithub <yv161993@gmail.com>
2025-07-25 18:07:49 +0530

    job seeker profile image updated

--- routes/jobSeekerProfileRoutes.js
+++ routes/jobSeekerProfileRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const upload = require('../middleware/upload'); 
 const { saveProfile, getProfile, updateProfileImage } = require('../controllers/jobSeekerController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 
 router.post('/create-jobSeeker-profile', verifyToken, saveProfile);
 router.get('/get-jobSeeker-profile', verifyToken, getProfile);
-router.patch('/jobSeeker-profile-image', verifyToken, updateProfileImage);
+
+router.patch('/jobSeeker-profile-image',
+     verifyToken,
+       upload.single("image"),
+      updateProfileImage);
 
 
 module.exports = router;
